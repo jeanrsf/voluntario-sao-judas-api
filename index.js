e8f9e9ca4d44b97c7e99e0445d942305aa8edd15
Jean <jeanrodrigosf@gmail.com>
2024-05-01 20:57:37 -0300

    Configurando cors

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
 const express = require ('express')
+const cors = require('cors')
+
 const { cadastroVoluntario } = require('./src/controladores/cadastro')
 const app = express()
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+app.use(cors({
+  origin: '*'
+}));
+
 
 app.use(express.json())
 
